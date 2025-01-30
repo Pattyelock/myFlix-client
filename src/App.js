f1c6ace5ff74314dfefcd3592c6142ac68414098
pattylock <pattycakesinfo@gmail.com>
2025-01-29 20:11:31 -0800

    Implemented state-based routing and navigation with React Router, updated main-view.jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Movies from './components/Movies';
+import ProfileView from './components/ProfileView';
 
 function App() {
   return (
@@ -9,7 +9,8 @@ function App() {
       <Routes>
         <Route path="/" element={<div>Home Page</div>} />
         <Route path="/movies" element={<Movies />} />
-        {/* <Route path="*" element={<div>404 Not Found</div>} /> */}
+        <Route path="/profile" element={<ProfileView />} />
+        <Route path="*" element={<div>404 Not Found</div>} />
       </Routes>
     </Router>
   );
